b1681935187f2cb00b06bdefb899296280ac4534
James White <james@jfwhite.org>
2023-08-31 16:46:17 -0700

    getting close with engage

--- resources/views/template-engage.blade.php
+++ resources/views/template-engage.blade.php
@@ -6,26 +6,26 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    <section class="engage-header px-6 py-12 sm:px-8 lg:px-16 xl:px-24">
+    <section class="engage-header mx-6 py-12 border-b border-black sm:px-8 lg:px-16 xl:px-24">
       <div class="max-w-1400 md:flex lg:mt-12">
         <aside class="mb-8 md:mr-12 lg:mr-auto lg:w-1/3">
         <h1 class="text-5xl font-bold uppercase leading-none
-                            font-rubik tracking-tighter mb-10 lg:leading-6 lg:mb-16 lg:mt-3 xl:text-6xl">{!! the_title() !!}</h1>
+                            font-rubik tracking-tighter mb-10 lg:mb-16 xl:text-6xl">{!! the_title() !!}</h1>
           @if (has_nav_menu('engage_navigation'))
             {!! wp_nav_menu(['theme_location' => 'engage_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
           @endif
         </aside>
-        <div class="md:px-6 lg:w-2/3 lg:pl-0 lg:ml-12">
+        <div class="md:px-6 lg:w-2/3 lg:pl-0 lg:ml-12 lg:mt-2">
           <div class="max-w-xl">@php(the_content())</div>
         </div>
       </div>
     </section>
-    <section class="px-6 py-12 sm:px-8 lg:px-16 xl:px-24">
+    <section class="mx-6 py-12 border-b border-black sm:px-8 lg:px-16 xl:px-24">
       @if($lpeStudentGroups)
       <div class="max-w-1400">
         <aside class="">
-        <h1 class="text-5xl font-bold uppercase leading-none 
-                            font-rubik tracking-tighter mb-10 lg:leading-6 lg:mb-16">Student<br/>Groups</h1>
+        <h1 class="text-4xl font-bold uppercase leading-none 
+                            font-rubik tracking-tighter mb-10 lg:leading-8 lg:mb-16">Student<br/>Groups</h1>
         </aside>
         <div class="max-w-3xl grid gap-8 sm:grid-cols-2 lg:grid-cols-3 lg:max-w-6xl">
           @foreach($lpeStudentGroups as $group)
@@ -64,20 +64,22 @@
     <section class="px-6 py-12 sm:px-8 lg:px-16 xl:px-24">
       <div class="max-w-1400 xl:flex">
         <aside class="max-w-sm xl:w-1/4">
-          <h1 class="text-5xl font-bold uppercase leading-none 
-                            font-rubik tracking-tighter mb-10 lg:leading-6 lg:mb-16">Start a Student<br/>Group</h1>
+          <h1 class="text-4xl font-bold uppercase leading-none 
+                            font-rubik tracking-tighter mb-10 lg:leading-8 lg:mb-16">Start a Student<br/>Group</h1>
         </aside>
-        <div class="mb-8 max-w-5xl xl:w-3/4 xl:pl-12">
+        <div class="speakers-blurb mb-8 max-w-5xl xl:w-3/4 xl:pl-12">
           {!! get_field('student_groups_blurb') !!}
+          <div class="block my-2">&nbsp;</div>
+          @include('components/generic-btn', [ 'url' => get_field('student_group_form'), 'copy' => 'Go To Form' ])
         </div>
       
       </div>
     </section>
     <section class="bg-tahini-500 px-6 py-12 sm:px-8 lg:px-16 xl:px-24">
       <div class="max-w-1400 xl:flex">
-        <aside class="max-w-lg lg:pr-6 xl:w-1/4 xl:mt-5">
-        <h1 class="text-5xl font-bold uppercase leading-none 
-                            font-rubik tracking-tighter mb-4 lg:leading-6 lg:mb-6">Speakers<br/>Bureau</h1>
+        <aside class="max-w-lg lg:pr-6 xl:w-1/4 xl:mt-3">
+        <h1 class="text-4xl font-bold uppercase leading-none 
+                            font-rubik tracking-tighter mb-4 lg:leading-8 lg:mb-6">Speakers<br/>Bureau</h1>
           <div class="mb-8">
             {!! get_field('speakers_blurb') !!}
           </div>
