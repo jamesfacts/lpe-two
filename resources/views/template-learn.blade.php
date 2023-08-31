a45476deb62ba4b9b9e24f653d241eb87cd407de
James White <james@jfwhite.org>
2023-08-31 14:51:01 -0700

    add meta data and style majority of content on learn

--- resources/views/template-learn.blade.php
+++ resources/views/template-learn.blade.php
@@ -70,29 +70,40 @@
           </div>
         </div>
       </div>
+      <div class="flex max-w-1400">
+        <div class="mt-10 mb-6">
+          @include('components/generic-btn', [ 'url' => home_url('/videos/'), 'copy' => 'View All Videos' ])
+        </div>
+      </div>
     </section>
-    <section>
-      <div class="max-w-1400">
-        <aside>
-          <h1>Syllabi</h1>
+    <section class="px-6 py-12 sm:px-8 lg:px-16 xl:px-24">
+      <div class="max-w-1400 xl:flex">
+        <aside class="max-w-lg lg:pr-6 xl:w-1/4 xl:mt-5">
+          <h1 class="text-5xl font-bold uppercase leading-none 
+                            font-rubik tracking-tighter mb-4 lg:leading-6 lg:mb-6">Syllabi</h1>
+          <div class="mb-8">
+            {!! get_field('syllabi_blurb') !!}
+          </div>
         </aside>
-        <div class="">
+        <div class="max-w-3xl mr-auto grid gap-8 sm:grid-cols-2 lg:grid-cols-3 lg:max-w-full xl:w-3/4 xl:pl-12">
           @if($lpeSyllabi)
             @foreach($lpeSyllabi as $lpeSyllabus)
-            <article class="max-w-sm mx-auto px-5 lg:px-0">
-              <header>
-                  <h2 class="entry-title my-3">
-                  <a href="{{ $lpeSyllabus->url }}" class="text-2xl font-bold uppercase leading-none hover:text-tahini-500 font-rubik tracking-tighter my-3 lg:leading-none lg:text-3xl">
-                      {!! $lpeSyllabus->name !!}
-                  </a>
-                  </h2>
-              <span class="font-necto block">{!! $lpeSyllabus->professor !!}</span>
-              <span class="font-necto block mb-4">{!! $lpeSyllabus->school !!}</span>
+            <article class="max-w-sm mx-auto flex flex-col justify-between">
+              <div class="">
+                <header>
+                    <h2 class="entry-title my-3">
+                    <a href="{{ $lpeSyllabus->url }}" class="text-2xl font-bold uppercase leading-none hover:text-tahini-500 font-rubik tracking-tighter my-3 lg:leading-none lg:text-3xl">
+                        {!! $lpeSyllabus->name !!}
+                    </a>
+                    </h2>
+                <span class="font-necto block">{!! $lpeSyllabus->professor !!}</span>
+                <span class="font-necto block mb-4">{!! $lpeSyllabus->school !!}</span>
 
-              </header>
+                </header>
 
-            <div class="entry-summary">
-              {!! $lpeSyllabus->excerpt !!}
+              <div class="entry-summary">
+                {!! $lpeSyllabus->excerpt !!}
+              </div>
             </div>
             <div class="my-3 flex flex-row justify-between" style="max-width: 13rem;">
               @include('components/generic-btn', [ 'url' => $lpeSyllabus->url, 'copy' => 'Learn More' ])
@@ -103,14 +114,53 @@
           @endif
         </div>
       </div>
+      <div class="flex max-w-1400">
+        <div class="lg:w-1/4"></div>
+        <div class="mt-10 mb-6 lg:w-3/4 lg:pl-auto lg:pl-12">
+          @include('components/generic-btn', [ 'url' => home_url('/syllabi/'), 'copy' => 'View All Syllabi' ])
+        </div>
+      </div>
     </section>
-    <section class="bg-tahini-500">
-      <div class="max-w-1400">
-        <aside>
-          Primers blurb
+    <section class="bg-tahini-500 px-6 py-12 sm:px-8 lg:px-16 xl:px-24">
+      <div class="max-w-1400 xl:flex">
+        <aside class="max-w-lg lg:pr-6 xl:w-1/4 xl:mt-5">
+        <h1 class="text-5xl font-bold uppercase leading-none 
+                            font-rubik tracking-tighter mb-4 lg:leading-6 lg:mb-6">Primers</h1>
+          <div class="mb-8 primers-blurb">
+            {!! get_field('primers_blurb') !!}
+          </div>
         </aside>
-        <div class="">
-          @dump($primerPosts)
+        <div class="max-w-3xl mr-auto grid gap-8 sm:grid-cols-2 lg:grid-cols-3 lg:max-w-full xl:w-3/4 xl:pl-12">
+          @if($primerPosts)
+            @foreach($primerPosts as $lpePrimer)
+              <article class="max-w-sm mx-auto flex flex-col justify-between">
+                <div class="">
+                  <header>
+                      <h2 class="entry-title my-3">
+                      <a href="{!! $lpePrimer->url !!}" class="text-2xl font-bold uppercase leading-none hover:text-white font-rubik tracking-tighter my-3 lg:leading-none lg:text-3xl">
+                        {!! $lpePrimer->name !!}
+                      </a>
+                      </h2>
+
+                  </header>
+
+                <div class="entry-summary">
+                  {!! $lpePrimer->excerpt !!}
+                </div>
+              </div>
+              <div class="my-3 flex flex-row justify-between primers-btn" style="max-width: 13rem;">
+                @include('components/generic-btn', [ 'url' => $lpePrimer->url, 'copy' => 'Learn More' ])
+                @include('components/generic-btn', [ 'url' => $lpePrimer->download_url, 'copy' => 'Download' ])
+              </div>
+            </article>
+            @endforeach
+          @endif
+        </div>
+      </div>
+      <div class="flex max-w-1400">
+        <div class="lg:w-1/4"></div>
+        <div class="primers-nav mt-10 mb-6 lg:w-3/4 lg:pl-auto lg:pl-12">
+          @include('components/generic-btn', [ 'url' => home_url('/primers/'), 'copy' => 'View All Primers' ])
         </div>
       </div>
     </section>
