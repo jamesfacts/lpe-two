482e81c61ed2e87be8778726115ea015650fd23c
James White <james@jfwhite.org>
2023-08-31 18:26:36 -0700

    i think the engage is g 2 g

--- resources/views/components/speakers-tax.blade.php
+++ resources/views/components/speakers-tax.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-center">
+<div class="speaker-topic-dropdown flex flex-row justify-center w-100 border border-black cursor-pointer hover:bg-black hover:text-white xl:w-60">
     <div
         x-data="{
             open: false,
@@ -22,7 +22,7 @@
         x-on:keydown.escape.prevent.stop="close($refs.button)"
         x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
         x-id="['dropdown-button']"
-        class="relative"
+        class="relative w-full mx-0 pl-2 mr-auto"
     >
         <!-- Button -->
         <button
@@ -31,12 +31,12 @@
             :aria-expanded="open"
             :aria-controls="$id('dropdown-button')"
             type="button"
-            class="flex items-center gap-2 bg-white px-5 py-2.5 rounded-md shadow"
+            class="flex w-full justify-between gap-2 px-1 py-1 text-sm uppercase"
         >
-            Options
+            Speaker Topics
  
             <!-- Heroicon: chevron-down -->
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-black" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
             </svg>
         </button>
@@ -49,10 +49,10 @@
             x-on:click.outside="close($refs.button)"
             :id="$id('dropdown-button')"
             style="display: none;"
-            class="absolute left-0 mt-2 w-40 rounded-md bg-white shadow-md"
+            class="absolute left-0 w-100 bg-white border border-gray-300"
         >
             @foreach($topics as $topic)
-                <a href="{!! $topic->url !!}" class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">
+                <a href="{!! $topic->url !!}" class="topic-item flex items-center gap-2 w-full px-4 py-1 uppercase text-left text-black text-sm underline hover:bg-gray-300 ">
                     {!! $topic->name !!}
                 </a>
             @endforeach
--- resources/views/template-engage.blade.php
+++ resources/views/template-engage.blade.php
@@ -6,68 +6,72 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    <section class="engage-header mx-6 py-12 border-b border-black sm:px-8 lg:px-16 xl:px-24">
-      <div class="max-w-1400 md:flex lg:mt-12">
-        <aside class="mb-8 md:mr-12 lg:mr-auto lg:w-1/3">
+    <section class="engage-header mx-6 py-12  sm:mx-8 lg:mx-16 xl:mx-24">
+      <div class="max-w-1400 border-b border-black md:flex lg:mt-12 xl:border-none">
+        <aside class="mb-8 md:mr-12 lg:mr-auto lg:w-1/4">
         <h1 class="text-5xl font-bold uppercase leading-none
                             font-rubik tracking-tighter mb-10 lg:mb-16 xl:text-6xl">{!! the_title() !!}</h1>
           @if (has_nav_menu('engage_navigation'))
             {!! wp_nav_menu(['theme_location' => 'engage_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
           @endif
         </aside>
-        <div class="md:px-6 lg:w-2/3 lg:pl-0 lg:ml-12 lg:mt-2">
-          <div class="max-w-xl">@php(the_content())</div>
+        <div class="md:px-6 lg:w-3/4 lg:pl-0 lg:ml-12 xl:ml-20 xl:pr-0 xl:border-b xl:border-black">
+          <div class="max-w-xl xl:mr-auto xl:ml-0 xl:pl-3">@php(the_content())</div>
         </div>
       </div>
     </section>
-    <section class="mx-6 py-12 border-b border-black sm:px-8 lg:px-16 xl:px-24">
+    <section class="mx-6 py-12 sm:mx-8 lg:mx-16 xl:mx-24">
       @if($lpeStudentGroups)
-      <div class="max-w-1400">
-        <aside class="">
-        <h1 class="text-4xl font-bold uppercase leading-none 
-                            font-rubik tracking-tighter mb-10 lg:leading-8 lg:mb-16">Student<br/>Groups</h1>
+      <div class="max-w-1400 xl:flex">
+        <aside class="xl:w-1/4">
+          &nbsp;
         </aside>
-        <div class="max-w-3xl grid gap-8 sm:grid-cols-2 lg:grid-cols-3 lg:max-w-6xl">
-          @foreach($lpeStudentGroups as $group)
-          <article class="w-full bg-teal-200 max-w-sm mx-auto px-5 lg:px-0">
-            @if(@isset($group->img_url))
-                @include('components/thumb-figure', [
-                'aspect_ratio' => '65%', 
-                'img_url' => $group->img_url, 
-                'url' => $group->url, 
-                'alt' => $group->alt
-                ])
-            @endif
+        <div class="max-w-3xl lg:max-w-6xl xl:w-3/4 xl:pl-16">
+          <h1 class="text-4xl font-bold uppercase leading-none 
+                            font-rubik tracking-tighter mb-10 lg:leading-8 lg:mb-16">Student<br/>Groups</h1>
+          <div class="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            @foreach($lpeStudentGroups as $group)
+              <article class="w-full max-w-sm mx-auto">
+                @if(@isset($group->img_url))
+                    @include('components/thumb-figure', [
+                    'aspect_ratio' => '65%', 
+                    'img_url' => $group->img_url, 
+                    'url' => $group->url, 
+                    'alt' => $group->alt
+                    ])
+                @endif
 
-          <header>
-            <h2 class="entry-title my-3">
-              <a href="{{ get_permalink() }}" class="text-2xl font-bold uppercase leading-none hover:text-tahini-500 font-rubik tracking-tighter my-3 lg:leading-none lg:text-3xl">
-                {!! $group->name !!}
-              </a>
-            </h2>
-            <span class="font-necto block mb-4">{!! $group->school !!}</span>
+                <header>
+                  <h2 class="entry-title my-3">
+                    <a href="{{ get_permalink() }}" class="text-2xl font-bold uppercase leading-none hover:text-tahini-500 font-rubik tracking-tighter my-3 lg:leading-none lg:text-3xl">
+                      {!! $group->name !!}
+                    </a>
+                  </h2>
+                  <span class="font-necto block mb-4">{!! $group->school !!}</span>
 
-          </header>
-        </article>
+                </header>
+              </article>
+            @endforeach
+          </div>
 
-          @endforeach
         </div>
       </div>
       <div class="flex max-w-1400">
-        <div class=""></div>
-        <div class="mt-10 mb-6">
+        <div class="xl:w-1/4">&nbsp;</div>
+        <div class="mt-10 mb-6 xl:pl-16">
           @include('components/generic-btn', [ 'url' => home_url('/student-groups/'), 'copy' => 'View All Student Groups' ])
         </div>
       </div>
       @endif
     </section>
-    <section class="px-6 py-12 sm:px-8 lg:px-16 xl:px-24">
+    <section class="px-6 mb-12 sm:px-8 lg:px-16 xl:px-24">
       <div class="max-w-1400 xl:flex">
         <aside class="max-w-sm xl:w-1/4">
-          <h1 class="text-4xl font-bold uppercase leading-none 
-                            font-rubik tracking-tighter mb-10 lg:leading-8 lg:mb-16">Start a Student<br/>Group</h1>
+          &nbsp;
         </aside>
-        <div class="speakers-blurb mb-8 max-w-5xl xl:w-3/4 xl:pl-12">
+        <div class="speakers-blurb mb-8 max-w-5xl border-t border-black pt-8 xl:w-3/4 xl:ml-20">
+          <h1 class="text-4xl font-bold uppercase leading-none max-w-sm
+                            font-rubik tracking-tighter mb-10 lg:leading-8 lg:mb-16">Start a Student<br/>Group</h1>
           {!! get_field('student_groups_blurb') !!}
           <div class="block my-2">&nbsp;</div>
           @include('components/generic-btn', [ 'url' => get_field('student_group_form'), 'copy' => 'Go To Form' ])
@@ -77,7 +81,7 @@
     </section>
     <section class="bg-tahini-500 px-6 py-12 sm:px-8 lg:px-16 xl:px-24">
       <div class="max-w-1400 xl:flex">
-        <aside class="max-w-lg lg:pr-6 xl:w-1/4 xl:mt-3">
+        <aside class="max-w-lg mb-16 lg:pr-6 xl:w-1/4 xl:mt-3">
           <h1 class="text-4xl font-bold uppercase leading-none 
                             font-rubik tracking-tighter mb-4 lg:leading-8 lg:mb-6">Speakers<br/>Bureau</h1>
           <div class="mb-8">
@@ -88,7 +92,7 @@
           @endif
 
         </aside>
-        <div class="max-w-3xl mr-auto grid gap-8 sm:grid-cols-2 lg:grid-cols-3 lg:max-w-full xl:w-3/4 xl:pl-12">
+        <div class="max-w-3xl mr-auto grid gap-8 sm:grid-cols-2 lg:grid-cols-3 lg:max-w-full xl:w-3/4 xl:pl-16">
           @if($lpeSpeakers)
             @foreach($lpeSpeakers as $lpeSpeaker)
               <article class="border-b border-black mb-5 pb-2">
@@ -108,7 +112,7 @@
       </div>
       <div class="flex max-w-1400">
         <div class="lg:w-1/4"></div>
-        <div class="speakers-nav mt-10 mb-6 lg:w-3/4 lg:pl-auto lg:pl-12">
+        <div class="speakers-nav mt-10 mb-6 lg:w-3/4 lg:pl-auto lg:pl-16">
           @include('components/generic-btn', [ 'url' => home_url('/speakers/'), 'copy' => 'View All Speakers' ])
         </div>
       </div>
