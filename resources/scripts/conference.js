2cca38077aad7482c9e79df76b800d532978ee28
James White <james@jfwhite.org>
2023-10-13 19:38:36 -0700

    yeaaaaaaa

--- resources/scripts/conference.js
+++ resources/scripts/conference.js
@@ -3,14 +3,33 @@
 export default {
     init() {
         const closePanelBtns = document.querySelectorAll('.rounded-panel.odd .expansion-toggle');
+        const closeEvenPanelBtns = document.querySelectorAll('.rounded-panel.even .expansion-toggle');
         const firstPlaceholder = document.querySelector('.grid-placeholder.rounded-panel'); // grab just the first placeholder
  
+        function scrollToPanel(scrollToItem) {
+            $('html, body').animate(
+              {
+                scrollTop: $(`#${scrollToItem}`).offset().top,
+              },
+              500,
+              'linear'
+            );
+        }
+
         for (const closePanelBtn of closePanelBtns) {
-            closePanelBtn.addEventListener('click', () => {
-            //   hideCards();
-            //   scrollToPanel(scrollItem);
-                console.log('eyyyyy');
+            closePanelBtn.addEventListener('click', (ev) => {
+                let target = ev.target;
+                let scrollItem = $(target).closest('.panel-card').attr('id');
+
                 firstPlaceholder.classList.toggle('hidden');
+                scrollToPanel(scrollItem);
+            });
+        }
+
+        for (const closeEvenPanel of closeEvenPanelBtns) {
+            closeEvenPanel.addEventListener('click', (ev) => {
+            //   scrollToPanel(scrollItem);
+                firstPlaceholder.classList.remove('hidden');
             });
         }
     }
