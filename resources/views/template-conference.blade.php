7de0db0bae411f4c4c0dae49a57aac5e1ed95a31
James White <james@jfwhite.org>
2024-01-02 19:08:23 -0800

    ok, got the conf description expanding

--- resources/views/template-conference.blade.php
+++ resources/views/template-conference.blade.php
@@ -5,15 +5,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="bg-black px-4 md:py-12 md:pt-8" x-data="{selected:null}">
+<div class="bg-black px-4 lg:py-12 lg:pt-8" x-data="{selected:null}">
   @while(have_posts()) @php(the_post())
-    <div class="max-w-7xl rounded-panel bg-center bg-cover p-4 md:p-12" style="background-image: url( {{ \Roots\asset('images/conf-texture-bg.png') }} );">
+    <div class="max-w-7xl rounded-panel bg-center bg-cover p-4 sm:pt-8 md:p-12" style="background-image: url( {{ \Roots\asset('images/conf-texture-bg.png') }} );">
         <div class="max-w-5xl">
-            <h1 class="text-4xl leading-9 font-bold font-tiempos tracking-tight lg:text-7xl lg:leading-23">{!! $title !!}</h1>
-            <h2 class="text-4xl mt-3 font-tiempos tracking-tight lg:text-7xl lg:leading-23">Spring 2023</h2>
+            <h1 class="text-4xl leading-9 font-bold font-tiempos tracking-tight sm:text-6xl sm:leading-16 md:text-7xl md:leading-18">{!! $title !!}</h1>
+            <h2 class="text-4xl font-tiempos tracking-tight sm:text-6xl sm:leading-16 md:text-7xl md:leading-18 lg:mt-3">Spring 2023</h2>
 
-            <div class="max-w-4xl mx-1 pr-4 mt-10 mb-12 lg:pr-8 lg:mt-16 conf-copy">
-                @php(the_content())
+            <div class="max-w-xl mx-1 pr-4 mt-10 mb-12 lg:max-w-4xl lg:pr-8 lg:mt-16 " x-data="{confExpanded:null}">
+                <div class="conf-copy lg:hidden">
+                  @php(the_content())
+                </div>
+                <div class="hidden lg:block">
+                  <div class="" x-show="!confExpanded">
+                    blah blah blah
+                  </div>
+
+                  <div class="grid grid-cols-2" x-show="confExpanded">
+                    <div class="conf-copy text-lg">
+                      @php(the_content())
+                    </div>
+                    
+                  </div>  
+                  <a href="javascript:void(0)" 
+                          class="border border-black rounded-full uppercase text-xs px-3 text-center py-2 inline-block hover:text-tahini-500 hover:border-tahini-500" 
+                          aria-label="Read / Close Full Conference Description" @click="confExpanded = ! confExpanded" x-text="confExpanded ? 'Close Description' : 'Learn More'">
+                          
+                  </a>
+                </div>
+
             </div>
         </div>
     </div>
