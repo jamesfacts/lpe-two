42910e09b63b389143e9a15c66245d14e107b3fb
James White <james@jfwhite.org>
2024-02-24 18:59:33 -0800

    whew.... these header heights

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,6 +1,6 @@
 <div class="page-header" style="margin-left: -2px;">
   @if(!empty($featuredHeaderImage))
-    <figure class="h-[420px] bg-center bg-no-repeat bg-cover xl:h-[39vh]" style="background-image: url( '{!! $featuredHeaderImage['url'] !!}' )">
+    <figure class="h-[300px] bg-center bg-no-repeat bg-cover " style="background-image: url( '{!! $featuredHeaderImage['url'] !!}' )">
     </figure>
   @else
     <span class="featured-image"></span>
