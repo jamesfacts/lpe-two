79be6798a2952841c791d4191db93facd62a6618
James White <james@jfwhite.org>
2024-02-25 16:46:38 -0800

    style emerging scholars

--- resources/views/components/scholar-item.blade.php
+++ resources/views/components/scholar-item.blade.php
@@ -1,8 +1,8 @@
 
 <article class="workshop-item relative z-0 break-inside-avoid pt-5 mx-2 border-black border-t">
-    <div class="bg-orange-600 mb-5">
-        <h2>{!! $item->workshop_title !!}</h2>
-        <span class="scholars">Scholars: {!! $item->workshop_scholars !!}</span>
-        <span class="readers">Readers: {!! $item->workshop_readers !!}</span>
+    <div class="mb-5">
+        <h2 class="font-tiempos text-2xl leading-7 pb-6">{!! $item->workshop_title !!}</h2>
+        <span class="text-xl leading-6 inline-block pb-3">Scholars: {!! $item->workshop_scholars !!}</span>
+        <span class="text-xl leading-6 inline-block pb-3">Readers: {!! $item->workshop_readers !!}</span>
     </div>
 </article>
--- resources/views/components/scholar-placeholder.blade.php
+++ resources/views/components/scholar-placeholder.blade.php
@@ -1,5 +1,11 @@
-<article class="workshop-item relative bg-yellow-200 z-0 mx-2 border-black border-t py-5">
-    <div class="bg-orange-600 mb-5">
-        <img src="{{ 'x' }}" >
+@php
+    $placeholderSrc = 'images/LPE_TextureRound' . random_int(2, 3) . '.png';
+
+@endphp
+
+
+<article class="workshop-item relative z-0 mx-2 border-black border-t py-5">
+    <div class="flex justify-center my-5">
+        <img src="{{ \Roots\asset($placeholderSrc) }}" >
     </div>
 </article>
